9d7b2305689b06887323f9fb4bd9fa15ee8774c3
Alpha-85 <ljungdahlt@gmail.com>
2021-12-31 11:39:53 +0100

    #3 Added Controller for recipes

---
+++ src/WebAppApi/Controllers/RecipeController.cs
@@ -0,0 +1,41 @@
+﻿using MediatR;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using System.Threading.Tasks;
+
+namespace RecipeApi.WebAppApi.Controllers;
+[ApiController]
+[Route("/api/v1[controller]")]
+public class RecipeController : ControllerBase
+{
+    private readonly IMediator _mediator;
+    private readonly ILogger<RecipeController> _logger;
+    public RecipeController(IMediator mediator, ILogger<RecipeController> logger)
+    {
+        _mediator = mediator;
+        _logger = logger;
+    }
+
+
+    /// <summary>
+    /// Asks the api to get random recipes based on preferences
+    /// </summary>
+    /// <param name="mealType"></param>
+    /// <param name="mainIngredient"></param>
+    /// <returns>List of random recipes</returns>
+
+    [HttpGet]
+    [ProducesResponseType(StatusCodes.Status200OK)]
+    [ProducesResponseType(StatusCodes.Status400BadRequest)]
+    public async Task<IActionResult> Get([FromQuery] string mealType, string mainIngredient)
+    {
+
+        //var recipeList = await _mediator.Send(new GetRecipesQuery(mealType, mainIngredient));
+
+        //if (recipeList is null)
+        //    return NotFound();
+
+        //return Ok(recipeList);
+    }
+}
