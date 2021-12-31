51c2a915c8d8bb9ccc7c2af8065d5efea277f34d
Alpha-85 <ljungdahlt@gmail.com>
2021-12-31 18:04:07 +0100

    #5 Added model/dto and controllers

---
+++ src/Application/Common/Models/Authentication/AuthenticateResponse.cs
@@ -0,0 +1,26 @@
+﻿
+
+using RecipeApi.Domain.Entities;
+using System.Text.Json.Serialization;
+
+namespace RecipeApi.Application.Common.Models.Authentication;
+
+public class AuthenticateResponse
+{
+    public int Id { get; set; }
+    public string FirstName { get; set; }
+    public string LastName { get; set; }
+    public string Username { get; set; }
+    public string JwtToken { get; set; }
+
+    [JsonIgnore] // refresh token is returned in http only cookie
+    public string RefreshToken { get; set; }
+
+    public AuthenticateResponse(User user, string jwtToken, string refreshToken)
+    {
+        Id = user.Id;
+        Username = user.UserName;
+        JwtToken = jwtToken;
+        RefreshToken = refreshToken;
+    }
+}
