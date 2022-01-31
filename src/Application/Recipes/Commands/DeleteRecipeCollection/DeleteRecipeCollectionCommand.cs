28dc94e07132cc0956500d3779957b8ca4bea50d
Alpha-85 <ljungdahlt@gmail.com>
2022-01-31 14:00:13 +0100

    DeleteCommands

---
+++ src/Application/Recipes/Commands/DeleteRecipeCollection/DeleteRecipeCollectionCommand.cs
@@ -0,0 +1,15 @@
+﻿using MediatR;
+
+namespace RecipeApi.Application.Recipes.Commands.DeleteRecipeCollection;
+
+public class DeleteRecipeCollectionCommand : IRequest<bool>
+{
+    public int UserId { get; }
+    public int CollectionId { get; }
+
+    public DeleteRecipeCollectionCommand(in int userId, in int collectionId)
+    {
+        UserId = userId;
+        CollectionId = collectionId;
+    }
+}
---
+++ src/Application/Recipes/Commands/DeleteRecipeCollection/DeleteRecipeCollectionCommandHandler.cs
@@ -0,0 +1,34 @@
+﻿using MediatR;
+using Microsoft.EntityFrameworkCore;
+using RecipeApi.Application.Common.Interfaces;
+
+namespace RecipeApi.Application.Recipes.Commands.DeleteRecipeCollection;
+
+public class DeleteRecipeCollectionCommandHandler : IRequestHandler<DeleteRecipeCollectionCommand, bool>
+{
+    private readonly IApplicationDbContext _context;
+
+    public DeleteRecipeCollectionCommandHandler(IApplicationDbContext context)
+    {
+        _context = context ?? throw new ArgumentNullException(nameof(context));
+    }
+
+    public async Task<bool> Handle(DeleteRecipeCollectionCommand request, CancellationToken cancellationToken)
+    {
+        var collection = await _context.RecipeCollections
+            .Include(x => x.RecipeDays)
+            .ThenInclude(x => x.Recipe)
+            .Where(u => u.UserId == request.UserId && u.Id == request.CollectionId)
+            .FirstOrDefaultAsync(cancellationToken);
+
+        if (collection is null)
+            return false;
+
+        _context.RecipeCollections.Remove(collection);
+        await _context.SaveChangesAsync(cancellationToken);
+
+        return true;
+
+
+    }
+}
