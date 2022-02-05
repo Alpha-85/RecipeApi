70549174aa5e48fe36968b8a2ce37fe88264abca
Alpha-85 <ljungdahlt@gmail.com>
2022-02-05 19:16:50 +0100

    Controller,MediatR,Models

---
+++ src/Application/Common/ViewModels/ShoppingIngredientViewModel.cs
@@ -0,0 +1,7 @@
+﻿namespace RecipeApi.Application.Common.ViewModels;
+
+public class ShoppingIngredientViewModel
+{
+    public string Name { get; set; }
+    public double Amount { get; set; }
+}
---
+++ src/Application/Recipes/Queries/GetRecipeIngredients/GetShoppingIngredientsHandler.cs
@@ -0,0 +1,12 @@
+﻿using MediatR;
+using RecipeApi.Application.Common.Models.Recipes.RecipeResponse;
+
+namespace RecipeApi.Application.Recipes.Queries.GetRecipeIngredients;
+
+internal class GetShoppingIngredientsHandler : IRequestHandler<GetShoppingIngredientsQuery, RecipeShoppingListResponse>
+{
+    public Task<RecipeShoppingListResponse> Handle(GetShoppingIngredientsQuery request, CancellationToken cancellationToken)
+    {
+        throw new NotImplementedException();
+    }
+}
