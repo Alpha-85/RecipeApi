fa8179c2568a9715ab75896947b5f59f8f7650b0
Alpha-85 <ljungdahlt@gmail.com>
2022-02-07 13:43:56 +0100

    Test

---
+++ tests/Application.IntegrationTests/Handlers/AddUserCommandHandlerTests.cs
@@ -0,0 +1,43 @@
+﻿using Application.IntegrationTests.Helpers;
+using FluentAssertions;
+using Microsoft.Extensions.Logging;
+using RecipeApi.Application.Users.Commands;
+using System.Threading;
+using System.Threading.Tasks;
+using Xunit;
+
+
+
+namespace Application.IntegrationTests.Handlers;
+
+public class AddUserCommandHandlerTests
+{
+    [Fact]
+    public async Task AddedUserHandlerShouldNotThrowException()
+    {
+        var handler = CreateSut();
+        var request = new AddUserCommand("Alfons", "1234");
+
+        var exception = await Record.ExceptionAsync(() => handler.Handle(request, CancellationToken.None));
+        Assert.Null(exception);
+    }
+
+    [Fact]
+    public async Task HandlerShouldReturnCorrectUser()
+    {
+        var handler = CreateSut();
+        var user = new AddUserCommand("Alfons", "1234");
+
+        var result = await handler.Handle(user, CancellationToken.None);
+
+        result.Should().BeTrue();
+    }
+
+    private static AddUserCommandHandler CreateSut()
+    {
+        var applicationContext = DbContextHelper.GetApplicationDbContext();
+        var logger = NSubstitute.Substitute.For<ILogger<AddUserCommandHandler>>();
+        var handler = new AddUserCommandHandler(applicationContext, logger);
+        return handler;
+    }
+}
---
+++ tests/Application.IntegrationTests/Helpers/DbContextHelper.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.EntityFrameworkCore;
+using RecipeApi.Infrastructure.Persistence;
+using System;
+
+namespace Application.IntegrationTests.Helpers;
+
+public static class DbContextHelper
+{
+    public static ApplicationDbContext GetApplicationDbContext()
+    {
+        DbContextOptionsBuilder optionsBuilder = new DbContextOptionsBuilder();
+        optionsBuilder.UseInMemoryDatabase(Guid.NewGuid().ToString());
+        ApplicationDbContext applicationDbContext = new ApplicationDbContext(optionsBuilder.Options);
+        return applicationDbContext;
+    }
+}
