54cae38ca1427344042b4b0a519bc2ec07ae4f89
Alpha-85 <ljungdahlt@gmail.com>
2022-02-18 14:44:10 +0100

    Removed refreshtoken from cookies and put it in respons instead

---
+++ tests/Application.UnitTests/Handlers/AddRecipeCommandHandlerTests.cs
@@ -0,0 +1,27 @@
+﻿using System.Threading;
+using Application.UnitTests.Helpers;
+using AutoMapper;
+using NSubstitute;
+using RecipeApi.Application.Common.Models.UserRecipes;
+using RecipeApi.Application.Recipes.Commands;
+using System.Threading.Tasks;
+using Xunit;
+
+namespace Application.UnitTests.Handlers;
+
+public class AddRecipeCommandHandlerTests
+{
+    [Fact(Skip = "Not done")]
+    public async Task HandlerShouldNotThrowException()
+    {
+        var applicationDbContext = DbContextHelper.GetApplicationDbContext();
+        var mapper = Substitute.For<IMapper>();
+        var recipe = RequestObjectBuilder.GetUserRecipeRequest();
+        var handler = new AddRecipeCommandHandler(applicationDbContext, mapper);
+        var request = new AddRecipeCommand(recipe);
+
+        var exception = await Record.ExceptionAsync(() => handler.Handle(request, CancellationToken.None));
+
+        Assert.Null(exception);
+    }
+}
