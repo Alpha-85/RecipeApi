61dd23f39da912aa72753b52b1241407d610ffd1
Alpha-85 <ljungdahlt@gmail.com>
2022-02-20 17:19:38 +0100

    MemoryCacheTests

--- tests/Application.UnitTests/Helpers/RequestObjectBuilder.cs
+++ tests/Application.UnitTests/Helpers/RequestObjectBuilder.cs
@@ -1,4 +1,6 @@
-﻿using RecipeApi.Application.Common.Models;
+﻿using System.Collections.Generic;
+using RecipeApi.Application.Common.Models;
+using RecipeApi.Application.Common.Models.SpoonResponse;
 using RecipeApi.Application.Common.Models.UserRecipes;
 using RecipeApi.Domain.Enums;
 
@@ -26,5 +28,57 @@ public static class RequestObjectBuilder
         };
 
         return request;
+    }
+
+    public static List<Recipe> GetRecipes()
+    {
+        var recipes = new List<Recipe>();
+
+        recipes.Add(new Recipe
+        {
+            Vegetarian = false,
+            Vegan = false,
+            GlutenFree = false,
+            DairyFree = false,
+            VeryHealthy = false,
+            Cheap = false,
+            VeryPopular = false,
+            Sustainable = false,
+            WeightWatcherSmartPoints = 0,
+            Gaps = "",
+            LowFodmap = false,
+            AggregateLikes = 0,
+            SpoonacularScore = 0,
+            HealthScore = 0,
+            CreditsText = "",
+            License = "",
+            SourceName = "",
+            PricePerServing = 0,
+            ExtendedIngredients = new List<ExtendedIngredient>
+            {
+                Capacity = 1
+            },
+            Id = 0,
+            Title = "",
+            ReadyInMinutes = 0,
+            Servings = 0,
+            SourceUrl = "",
+            Image = "",
+            ImageType = "",
+            Summary = "",
+            Cuisines = new List<string>(){""},
+            DishTypes = new List<string>(){""},
+            Diets = new List<string>(){""},
+            Occasions = new List<object>(),
+            Instructions = "",
+            AnalyzedInstructions = new List<AnalyzedInstruction>
+            {
+                Capacity = 1
+            },
+            OriginalId = new object(),
+            SpoonacularSourceUrl = ""
+        });
+
+        return recipes;
     }
 }
---
+++ tests/Application.UnitTests/Services/MemoryCacheServiceTests.cs
@@ -0,0 +1,55 @@
+﻿
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Threading.Tasks;
+using Application.UnitTests.Helpers;
+using FluentAssertions;
+using Microsoft.Extensions.Caching.Memory;
+using NSubstitute;
+using RecipeApi.Application.Common.Interfaces;
+using RecipeApi.Application.Common.Models.SpoonResponse;
+using RecipeApi.Application.Services;
+using RecipeApi.Domain.Enums;
+using Xunit;
+
+namespace Application.UnitTests.Services;
+
+public class MemoryCacheServiceTests
+{
+    [Fact]
+    public async Task MemoryCacheServiceShouldNotThrowException()
+    {
+        var memory = Substitute.For<IMemoryCache>();
+        var spoonAdapter = Substitute.For<ISpoonAdapter>();
+        const string query = "beef";
+
+        var sut = new MemoryCacheService(memory, spoonAdapter);
+        spoonAdapter.GetRandomRecipesAsync(query).Returns(new List<Recipe>());
+      
+
+        var exception = await Record.ExceptionAsync(() => sut.GetCachedRecipes(PreferenceType.Beef,query));
+
+        Assert.Null(exception);
+    }
+
+    [Fact]
+    public async Task GetCachedRecipesShouldNotReturnNull()
+    {
+        var memory = Substitute.For<IMemoryCache>();
+        var spoonAdapter = Substitute.For<ISpoonAdapter>();
+        var recipes = RequestObjectBuilder.GetRecipes();
+        const string query = "beef";
+        var category = PreferenceType.Beef;
+        var sut = new MemoryCacheService(memory, spoonAdapter);
+        memory.Get(category).Returns(true,recipes);
+        spoonAdapter.GetRandomRecipesAsync(query).Returns(new List<Recipe>());
+
+
+        var result = await sut.GetCachedRecipes(category, query);
+
+        result.Should().NotBeNull();
+
+    }
+
+
+}
