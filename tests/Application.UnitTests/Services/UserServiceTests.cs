86187229fff281ca4514c1be141f1e8bb0bf96d2
Alpha-85 <ljungdahlt@gmail.com>
2022-02-25 14:55:50 +0100

    UserServiceTests

---
+++ tests/Application.UnitTests/Services/UserServiceTests.cs
@@ -0,0 +1,101 @@
+﻿
+using Application.UnitTests.Helpers;
+using FluentAssertions;
+using Microsoft.Extensions.Options;
+using NSubstitute;
+using RecipeApi.Application.Common.Interfaces;
+using RecipeApi.Application.Common.Settings;
+using RecipeApi.Application.Services;
+using RecipeApi.Domain.Entities;
+using System;
+using System.Security.Cryptography;
+using System.Threading;
+using System.Threading.Tasks;
+using Xunit;
+
+namespace Application.UnitTests.Services;
+
+public class UserServiceTests
+{
+    [Fact]
+    public async Task UserServiceShouldNotThrowException()
+    {
+        var appSettings = Substitute.For<IOptions<AppSettings>>();
+        var jwtService = Substitute.For<IJwtService>();
+        appSettings.Value.Returns(SettingsHelper.GetAppSettings());
+        var user = UserObjectBuilder.GetDefaultUser();
+
+        var sut = new UserService(jwtService, appSettings);
+
+        var exception = await Record.ExceptionAsync(() => sut.RemoveOldRefreshTokens(user));
+
+        Assert.Null(exception);
+
+    }
+
+    [Fact]
+    public void  UserServiceShouldRemoveOldRefreshTokens()
+    {
+        var appSettings = Substitute.For<IOptions<AppSettings>>();
+        var jwtService = Substitute.For<IJwtService>();
+        appSettings.Value.Returns(SettingsHelper.GetAppSettings());
+        var user = UserObjectBuilder.GetDefaultUser();
+
+        var sut = new UserService(jwtService, appSettings);
+
+        var result = sut.RemoveOldRefreshTokens(user);
+
+        result.Should().Be(Task.CompletedTask);
+    }
+
+    [Fact]
+    public void UserServiceShouldRevokeDescendantRefreshTokens()
+    {
+        var appSettings = Substitute.For<IOptions<AppSettings>>();
+        var jwtService = Substitute.For<IJwtService>();
+        appSettings.Value.Returns(SettingsHelper.GetAppSettings());
+        var user = UserObjectBuilder.GetDefaultUser();
+
+        var sut = new UserService(jwtService, appSettings);
+
+        var result = sut.RevokeDescendantRefreshTokens(new RefreshToken(), user, "192.168.0.1", "Revoked");
+
+        result.Should().Be(Task.CompletedTask);
+    }
+
+    [Fact]
+    public async Task UserServiceShouldRotateRefreshToken()
+    {
+        var appSettings = Substitute.For<IOptions<AppSettings>>();
+        var jwtService = Substitute.For<IJwtService>();
+        appSettings.Value.Returns(SettingsHelper.GetAppSettings());
+        var value = appSettings.Value;
+        var refreshToken = GetRefreshToken(value.Secret);
+
+        jwtService.GenerateRefreshToken("192.168.0.1", CancellationToken.None).Returns(GetRefreshToken(value.Secret));
+
+        var sut = new UserService(jwtService, appSettings);
+
+        var result = await sut.RotateRefreshToken(refreshToken, "192.168.0.1", CancellationToken.None);
+
+        result.Should().BeOfType<RefreshToken>();
+        result.RevokedByIp.Should().NotBeSameAs(refreshToken.RevokedByIp);
+    }
+
+    private static RefreshToken GetRefreshToken(string secret)
+    {
+        byte[] bytes = new byte[64];
+        var rng = RandomNumberGenerator.Create();
+        rng.GetBytes(bytes);
+
+        var refreshToken = new RefreshToken
+        {
+            Token = Convert.ToBase64String(bytes),
+            Expires = DateTime.MinValue,
+            Created = DateTime.MinValue,
+            CreatedByIp = "192.168.0.133"
+        };
+
+        return refreshToken;
+    }
+}
