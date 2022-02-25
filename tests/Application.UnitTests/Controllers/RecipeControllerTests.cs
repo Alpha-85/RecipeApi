856f0bae075003cafa6d24c1e12d91e02c6bd8af
Alpha-85 <ljungdahlt@gmail.com>
2022-02-25 19:37:26 +0100

    Controller-tests

---
+++ tests/Application.UnitTests/Controllers/RecipeControllerTests.cs
@@ -0,0 +1,26 @@
+﻿using MediatR;
+using Microsoft.Extensions.Logging;
+using NSubstitute;
+using RecipeApi.Application.Common.Models.Recipes;
+using RecipeApi.WebAppApi.Controllers;
+using System.Threading.Tasks;
+using Xunit;
+
+namespace Application.UnitTests.Controllers;
+
+public class RecipeControllerTests
+{
+    [Fact]
+    public async Task SpoonAdapterShouldNotThrowException()
+    {
+        var mediator = Substitute.For<IMediator>();
+        var logger = Substitute.For<ILogger<RecipeController>>();
+
+        var sut = new RecipeController(mediator, logger);
+
+        var exception = await Record.ExceptionAsync(() => sut.Get(new RecipeRequest()));
+
+        Assert.Null(exception);
+
+    }
+}
