30fd5af72e32080d5b6e1d5571a18f6392b42502
Alpha-85 <ljungdahlt@gmail.com>
2022-02-25 14:55:50 +0100

    UserServiceTests

---
+++ tests/Application.UnitTests/Helpers/SettingsHelper.cs
@@ -0,0 +1,18 @@
+﻿
+using RecipeApi.Application.Common.Settings;
+
+namespace Application.UnitTests.Helpers;
+
+public static class SettingsHelper
+{
+    public static AppSettings GetAppSettings()
+    {
+        var appSetting = new AppSettings()
+        {
+            Secret = "d3fl423d3fl423d3fl423d3fl423d3fl423d3fl423d3fl423d3fl423d3fl423d3fl423d3fl423d3fl423",
+            RefreshTokenTTL = 7
+        };
+
+        return appSetting;
+    }
+}
